0aa95b20c2285b6ac7c0f093116ae0c51669c41b
Haridas250503 <haridasshinde314@gmail.com>
2025-12-24 00:54:33 +0530

    add both files

--- src/main/java/com/model/Student.java
+++ src/main/java/com/model/Student.java
@@ -8,30 +8,8 @@ public class Student {
 	private double percentage;
 	
 	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public double getPercentage() {
-		return percentage;
-	}
-	public void setPercentage(double percentage) {
-		this.percentage = percentage;
-	}
+
+
 	
 	
 
